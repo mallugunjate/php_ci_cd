5f6e5586bcfba5b0cd99dc376be42b86af0eb82f
mallappa <mallappa@nalashaa.com>
2024-03-07 18:17:06 +0530

    Test update 2

--- index.php
+++ index.php
@@ -12,6 +12,7 @@
         <h1> Hello WOrld!</h1>
         <?php
         echo "Hello World! Lets build CI/CD pipeline - modified";
+        echo "<br>";
         echo "Welcome to CI/CD demo";
         ?>
     </div>
