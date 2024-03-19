27dd9c6fecb33633268cca8eaaa4972b07ed64ea
mallappa <mallappa@nalashaa.com>
2024-03-19 13:08:16 +0530

    added css files

--- index.php
+++ index.php
@@ -5,13 +5,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CI/CD Pipeline</title>
+    <link rel="stylesheet" href="styles.css">
 </head>
 
-<body>
+<body class="body-background">
     <div>
         <h1> Hello World!</h1>
         <?php
-        echo "Hello World! Lets build CI/CD pipeline - Modified content";
+        echo "<div class='hello-world'>Hello World! Lets build CI/CD pipeline</div>";
         echo "<br>";
         echo "Welcome to CI/CD demo";
         echo phpinfo();
